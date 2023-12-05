f0fd1d684d4540f779c56655ee69666694366eb3
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-12-05 20:55:35 +0300

    Add files via upload

---
+++ cs/Lab2 Prototype/Program.cs
@@ -0,0 +1,35 @@
+﻿//створ нові обєкти викор існуючий обєкт як прототип 
+//для клонування книг де є прототип (оригінал) та нові обєкти (копії)
+using System;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public Book Clone()
+    {
+        return new Book(Title, Author);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book bookPrototype = new Book("Sample Title", "Sample Author");
+
+        // Клонування книги
+        Book newBook = bookPrototype.Clone();
+        newBook.Title = "New Title";
+        newBook.Author = "New Author";
+
+        Console.WriteLine(newBook.Title + " by " + newBook.Author);
+    }
+}
